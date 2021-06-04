86ddb459ad06a2394ab3dd044779e5f0187e96c5
Rojar Smith <rojarsmith@gmail.com>
2021-06-04 22:23:07 +0800

    Add the new empty project for service discover and registration.

---
+++ backend/service-registration/src/main/java/io/aext/service/registration/ServiceRegistrationApplication.java
@@ -0,0 +1,13 @@
+package io.aext.service.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationApplication.class, args);
+	}
+
+}
---
+++ backend/service-registration/src/test/java/io/aext/service/registration/ServiceRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aext.service.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
