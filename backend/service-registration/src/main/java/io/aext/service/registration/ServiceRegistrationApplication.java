4999de2353dd3c93ab43e5dbe609a67ec23f87f4
Rojar Smith <rojarsmith@gmail.com>
2021-06-04 23:38:47 +0800

    Add Eureka server.

--- backend/service-registration/src/main/java/io/aext/service/registration/ServiceRegistrationApplication.java
+++ backend/service-registration/src/main/java/io/aext/service/registration/ServiceRegistrationApplication.java
@@ -2,7 +2,9 @@ package io.aext.service.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistrationApplication {
 
---
+++ backend/service-registration/src/main/resources/templates/simpleMessage.ftl
@@ -0,0 +1,9 @@
+<html>
+<body>
+<div>
+    <h1>Backend Service Status Notice</h1>
+    <br/><br/>
+    <br/>${msg}<br/>
+</div>
+</body>
+</html>
