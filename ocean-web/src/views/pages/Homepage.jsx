77ed3e8c3d4677d6c146ddebc0c07ba27463cbbe
Rojar Smith <rojarsmith@gmail.com>
2021-07-04 17:39:36 +0800

    Add ocean-web for front end web site and integrate tradio style.

---
+++ ocean-web/src/views/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React, { } from 'react';
+import Header1 from '../../components/Header/Header1';
+
+function Homepage() {
+    return (
+        <>
+            <Header1 />
+        </>
+    )
+}
+
+export default Homepage;
